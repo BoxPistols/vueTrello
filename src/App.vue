7f9234dc8dfff7f7ce3c6b78264da73b830dc4b0
ait <ait@ex.in>
2020-04-29 08:41:25 +0900

    [List] set store

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <Board />
-    <ListAdd />
+    <!-- <ListAdd /> -->
   </div>
 </template>
 
 <script>
 import Board from './components/Board.vue'
-import ListAdd from './components/ListAdd.vue'
+// import ListAdd from './components/ListAdd.vue'
 
 export default {
   name: 'App',
   components: {
     Board,
-    ListAdd,
+    // ListAdd,
   },
 }
 </script>
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,14 +3,20 @@
     <header>my Trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <ListAdd />
     </main>
   </div>
 </template>
 
 <script>
+import ListAdd from './ListAdd.vue'
+
 export default {
   name: '',
   data: () => ({}),
+  components: {
+    ListAdd,
+  },
 }
 </script>
 
